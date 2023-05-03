74569b20e35587aac610463a3b78f4a13c502b60
Lucas Schuber <lucas.hammarstrand@hotmail.com>
2023-05-03 11:09:56 +0200

    commit

--- includes/nav.php
+++ includes/nav.php
@@ -1,12 +1,6 @@
-<form method="POST">
-    <input type="text" id="search" name="search" placeholder="Search..">
-    <button class="" type="submit"><a>Sök &nbsp;</a></button><br><br>
-    <?php
-
-    
-
-
-    ?>
+<form method="POST" class="search">
+    <input class="search-bar" type="text" id="search" name="search" placeholder="Search..">
+    <button class="search-btn" type="submit"><a>Sök &nbsp;</a></button><br><br>
 </form>
 <div class="topnav container">
     <div class="left-menu" id="myTopnav">
