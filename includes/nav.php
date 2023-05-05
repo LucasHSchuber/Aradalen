b1cef7df03540ecfacf3ff3f098f44556722ad7e
Lucas Schuber <lucas.hammarstrand@hotmail.com>
2023-05-05 08:29:36 +0200

    nav #active

--- includes/nav.php
+++ includes/nav.php
@@ -1,11 +1,25 @@
-
+<?php
+$pages = array(
+    "Hem" => "index.php",
+    "Om sidan" => "inventory.php",
+    "Kontakt" => "list.php"
+);
+?>
 <form method="POST" class="search">
     <input class="search-bar" type="text" id="search" name="search" placeholder="Sök..">
     <button class="search-btn" type="submit1"><a><i class="fa-solid fa-magnifying-glass"></i></a></button><br><br>
 </form>
 
-    <div class="menu topnav" id="myTopnav">
-        <a href="index.php">Lägg till</a>
-        <a href="inventory.php">Inventory</a>
-        <a href="list.php">Lista</a>
-    </div>
+<div class="menu topnav" id="myTopnav">
+    <?php
+    foreach ($pages as $page => $page_filename) {
+        $current = basename($_SERVER['SCRIPT_FILENAME']);
+        if ($page_filename == $current) {
+            echo "<a href='$page_filename' class='active'>$page</a>\n";
+        } else {
+            echo "<a href='$page_filename'>$page</a>\n";
+        }
+    }
+    ?>
+
+</div>
